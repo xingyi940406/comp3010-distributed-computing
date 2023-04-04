b6c9a30d7d02cfff8b38e83b5edd985cfeeeb06e
xiaoran-MENG <xrmeng720@gmail.com>
2023-04-03 20:24:25 -0500

    Set up TCP and UDP sockets

--- 3010-consensus/server.py
+++ 3010-consensus/server.py
@@ -5,30 +5,53 @@ import select
 from utils import extractCommand
 
 class Server:
-    def __init__(self, port, host = 'localhost'):
-        self.port = port
-        self.host = host
+    
+    def createSockets(self):
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        peerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.configSocket(clientSocket)
+        self.configSocket(peerSocket)
+        clientSocket.listen(5)
+        self.logSocketsInfo(clientSocket, peerSocket)
+        return (clientSocket, peerSocket)
+    
+    def logSocketsInfo(self, clientSocket, peerSocket):
+        clientPort = clientSocket.getsockname()[1]
+        peerPort = peerSocket.getsockname()[1]
+        host = socket.gethostname()
+        print(f"Client | TCP | Port {clientPort} | Host {host}")
+        print(f"Peer   | UDP | Port {peerPort} | Host {host}")
+         
+    def configSocket(self, s):
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setblocking(False)
+        s.bind(('', 0))
         
     def start(self):
-        print(self.host + ':' + str(self.port))
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as peerSocket:
-            peerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            peerSocket.bind((self.host, self.port))
-            peerSocket.setblocking(False)
-            peerSocket.listen()
-
-            inputs = [peerSocket]
-            clients = []
-            outputs = []
-            
+        clientSocket, peerSocket = self.createSockets()
+        
+        inputs = [clientSocket, peerSocket]
+        clients = []
+        outputs = []
+        
+        with clientSocket, peerSocket:
             while True:
                 try:
-                    readable, writable, exceptional = select.select(inputs + clients, outputs, inputs + clients, 3)
+                    readable, writable, exceptional = select.select(
+                        inputs + clients, 
+                        outputs, 
+                        inputs + clients, 
+                        3)
+                    
                     for r in readable:
-                        if r is peerSocket:
+                        if r is clientSocket:
                             conn, addr = r.accept()
+                            print('Connected from: ', addr)
                             conn.setblocking(False)
                             clients.append(conn)
+                        elif r is peerSocket:
+                            data, addr = r.recvfrom(1024)
+                            print(f"UDP message from {addr}: {data.decode()}")
                         elif r in clients:
                             request = r.recv(1024)
                             if not request or len(request) == 0:
@@ -40,7 +63,7 @@ class Server:
                                 response = 'Received command: ' + data
                                 r.sendall(response.encode())
                         else:
-                            print('Readable not found')
+                            print('Readable not found')   
                 except socket.timeout as e:
                     print('Time out')
                 except KeyboardInterrupt as e:
@@ -49,17 +72,7 @@ class Server:
                     print(e)
 
 def main():
-    args = sys.argv
-    
-    if len(args) == 3:
-        port = int(args[1])
-        host = args[2]
-        Server(port, host).start()
-    elif len(args) == 2:
-        port = int(args[1])
-        Server(port).start()
-    else:
-        print('Invalid args')
+    Server().start()
 
 if __name__ == "__main__":
     main()
